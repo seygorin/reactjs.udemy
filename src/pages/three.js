ad187ce879a1cd36f65cefaaeb36db70b830a27f
Sergey Gorin <seygorin@gmail.com>
2021-08-20 11:08:16 +0500

    build fixes

--- src/pages/three.js
+++ src/pages/three.js
@@ -4,21 +4,22 @@ import AddUser from '../components/Users/AddUser';
 import UsersList from '../components/Users/UsersList';
 
 const Three = () => {
-  const [usersList, setUsersList] = useState([]);
+	const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (userName, userAge) => {
-     setUsersList((prevUsersList) => {
-       
-       return [... prevUsersList, ( {name: userName, age: userAge, id: Math.random().toString()} )];
-     });
-  }
-  return (
-    <div>
-        <AddUser onAddUser={addUserHandler} />
-        <UsersList users={usersList} />
-    </div>
-
-  );
+	const addUserHandler = (userName, userAge) => {
+		setUsersList((prevUsersList) => {
+			return [
+				...prevUsersList,
+				{ name: userName, age: userAge, id: Math.random().toString() },
+			];
+		});
+	};
+	return (
+		<div>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
+		</div>
+	);
 };
 
 export default Three;
